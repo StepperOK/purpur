8b854301e1a3a18a72820ea2b3bb1448a3b114ef
Vladimir <parnuk1996@gmail.com>
2021-11-21 21:39:15 +0300

    fix bugs

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,51 +1,132 @@
 import React, { useState } from "react";
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 import logo from "../../image/logo.png";
 import "./navbar.styles.scss";
+import { Link } from "react-router-dom";
+import "reactjs-popup/dist/index.css";
 
 function Navbar() {
-  const [showPopup, setShowPopup] = useState(false);
-
-const togglePopup = () => {
-  setShowPopup(!showPopup);
-};
-
-const closeModal = (e) => {
-  console.log(e.target.className);
-  if (e.target.className === "popup") {
-    togglePopup();
-  }
-};
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
 
   return (
-    <div className="navbar">
-      <div>
-        <img src={logo} alt="Logo of Website" />
-        <label>somyka</label>
-      </div>
-
-      <nav>
-        <li>Главная</li>
-        <li>О нас</li>
-        <li>Проекты</li>
-        <li>Преимущества</li>
-        <button className="open" onClick={togglePopup}>
-        Связаться
-        </button>
-        {showPopup ? (
-          <div className="popup" onClick={(e) => closeModal(e)}>
-            <div className="popup_inner">
-              <h2>Контакты</h2>
-              <div className="close" onClick={togglePopup}>
-                Х
+    <div className="header">
+      <div className="toolbar"></div>
+      <div className="navbar">
+        <div>
+          <img src={logo} alt="Logo of Website" />
+          <label>somyka</label>
+        </div>
+        <nav>
+          <div className="button">
+            <button
+              type="button"
+              className="button"
+              onClick={() => setOpen((o) => !o)}
+            >
+              Контакты
+            </button>
+            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+              <div className="modal">
+                <span className="modal_exit">
+                  <svg
+                    type="button"
+                    onClick={closeModal}
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M 0,0 L 20,20 M 20,0 L 0,20" stroke="black" />
+                    <defs>
+                      <clipPath id="clip0">
+                        <rect width="24" height="24" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </span>
+                <div className="modal__title">
+                  <h1>Наши контакты</h1>
+                </div>
+                <div className="modal__text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Iste, praesentium aspernatur. Laudantium natus est numquam
+                  veniam dolorem nobis et quibusdam. Quisquam atque a iure hic
+                  accusantium eum voluptas ipsa voluptatum.
+                </div>
+                <div className="button">
+                  <button
+                    type="button"
+                    className="modal_btn"
+                    onClick={closeModal}
+                  >
+                    Maybe later
+                  </button>
+                </div>
               </div>
-            </div>
+            </Popup>
           </div>
-        ) : null}
-      </nav>
+
+
+          
+            <li
+              type="text"
+              className="button"
+              onClick={() => setOpen((o) => !o)}
+            >
+              Ваш город
+            </li>
+            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+              <div className="modal">
+                <span className="modal_exit">
+                  <svg
+                    type="button"
+                    onClick={closeModal}
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M 0,0 L 20,20 M 20,0 L 0,20" stroke="black" />
+                    <defs>
+                      <clipPath id="clip0">
+                        <rect width="24" height="24" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </span>
+                <div className="modal__title">
+                  <h1>Наши контакты</h1>
+                </div>
+                <div className="modal__text">
+                  Lorem 
+                </div>
+                <div className="button">
+                  <button
+                    type="button"
+                    className="modal_btn"
+                    onClick={closeModal}
+                  >
+                    Maybe later
+                  </button>
+                </div>
+              </div>
+            </Popup>
+          
+
+        </nav>
+        <nav>
+          <li>Главная</li>
+          <li>О нас</li>
+          <li>Проекты</li>
+          <li>Преимущества</li>
+        </nav>
+      </div>
     </div>
   );
-};
+}
 
 export default Navbar;
